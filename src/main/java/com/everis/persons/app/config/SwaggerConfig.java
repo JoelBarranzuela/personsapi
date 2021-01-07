8aa3b6c56d88a6721e21b576040ec0fec5374a7e
Joel Barranzuela <joejoel0512@gmail.com>
2021-01-07 04:45:32 -0500

    listo para azure

--- src/main/java/com/everis/persons/app/config/SwaggerConfig.java
+++ src/main/java/com/everis/persons/app/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.everis.persons.app.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -10,6 +11,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+@Configuration
 public class SwaggerConfig {
   
   @Bean
---
+++ target/classes/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO person_response( document_number, finger_print, black_list) VALUES ('10000000',TRUE,FALSE);
+INSERT INTO person_response( document_number, finger_print, black_list) VALUES ('10000001',FALSE,FALSE);
+INSERT INTO person_response( document_number, finger_print, black_list) VALUES ('10000002',TRUE,TRUE);
+
+
